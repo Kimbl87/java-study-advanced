7fcaf103adadc8498d704185cbdc093833bcbf85
Денис <ivanovds87@mail.ru>
2022-11-10 12:20:29 +0300

    task 1 - fin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,26 @@
 import java.util.Arrays;
 import java.util.Scanner;
-
 /*
 Калькулятор
-В методе main сложение двух чисел
-Вывод - форматирование
-Складываем дабл
-Округление до 4 знаков после запятой
+ - В методе main сложение двух чисел
+ - Вывод - форматирование
+ - Складываем дабл
+ - Округление до 4 знаков после запятой
  */
+
+/**
+ * @author Иванов Денис
+ * @see Operations#add(double, double) Сложение
+ * @see Operations#subtract(double, double) Вычитание
+ * @see Operations#multiply(double, double) Умножение
+ * @see Operations#divide(double, double) Деление
+ */
+
+
 public class Main {
     public static void main(String[] args) {
         //Определяем переменные
         Scanner sc = new Scanner(System.in);
-        double result;
         System.out.println("Введите число 'A'");
         double a = sc.nextDouble();
         System.out.println("Введите один из знаков: +, -, *, /");
@@ -22,16 +30,16 @@ public class Main {
         //Выполняем действие по введенному оператору
         switch (operation) {
             case "+":
-                System.out.printf("%.4f + %.4f = %.4f", a, b, add(a, b));
+                System.out.printf("%.4f + %.4f = %.4f", a, b, Operations.add(a, b));
                 break;
             case "-":
-                System.out.printf("%.4f - %.4f = %.4f", a, b, subtract(a, b));
+                System.out.printf("%.4f - %.4f = %.4f", a, b, Operations.subtract(a, b));
                 break;
             case "*":
-                System.out.printf("%.4f * %.4f = %.4f", a, b, multiply(a, b));
+                System.out.printf("%.4f * %.4f = %.4f", a, b, Operations.multiply(a, b));
                 break;
             case "/":
-                System.out.printf("%.4f / %.4f = %.4f", a, b, divide(a, b));
+                System.out.printf("%.4f / %.4f = %.4f", a, b, Operations.divide(a, b));
                 break;
             default:
                 System.err.printf("Введен некорректный оператор. Ожидается: \u001B[1m+ -  * /\033[0m; введено: \u001B[1m%s", operation);
@@ -41,21 +49,5 @@ public class Main {
 
 
     }
-
-    private static double add(double a, double b) {
-        return (a + b);
-    }
-
-    private static double subtract(double a, double b) {
-        return 0;
-    }
-
-    private static double multiply(double a, double b) {
-        return 0;
-    }
-
-    private static double divide(double a, double b) {
-        return 0;
-    }
-
 }
+
---
+++ src/main/java/Operations.java
@@ -0,0 +1,25 @@
+public class Operations {
+
+    /**
+     * @param a первое слагаемое
+     * @param b второе слогаемое
+     * @return результат сложения
+     */
+    public static double add(double a, double b) {
+        return (a + b);
+    }
+
+    public static double subtract(double a, double b) {
+        return 0;
+    }
+
+    public static double multiply(double a, double b) {
+        return 0;
+    }
+
+    public static double divide(double a, double b) {
+        return 0;
+    }
+
+}
+
