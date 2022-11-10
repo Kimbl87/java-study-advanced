02fd9c90ef824145eb050f020a75fe1838fc4b0d
Денис <ivanovds87@mail.ru>
2022-11-10 12:26:09 +0300

    task 1 - fin

--- src/main/java/Operations.java
+++ src/main/java/Operations.java
@@ -5,18 +5,23 @@ public class Operations {
      * @param b второе слогаемое
      * @return результат сложения
      */
+
+    //Метод сложения
     public static double add(double a, double b) {
         return (a + b);
     }
 
+    //Метод выитания
     public static double subtract(double a, double b) {
         return 0;
     }
 
+    //Метод умножения
     public static double multiply(double a, double b) {
         return 0;
     }
 
+    //Метод деления
     public static double divide(double a, double b) {
         return 0;
     }
